eb5c25cd3081e053f00f2aa6d6db6f67d0c6fc4e
Roberto Guerra <prosaka@hotmail.com>
2022-03-12 19:56:10 -0300

    integrando o stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,8 +29,9 @@ export default function Home() {
           <p>
             Get acess to all publications
             <br />
+            <span>for {product.amount} month </span>
           </p>
-          <span>for $9.90 month </span>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image
@@ -33,3 +44,22 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // usar o expand: ['product'] para retornar os dados do produto
+  const price = await stripe.prices.retrieve("price_1Kcd50B4PxTzH8QgtIxCwxRh");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
